82856291af98428e074eb8ce22213d8dfd05c8ed
metaabhay <meta29abhay@gmail.com>
2026-09-10 23:55:52 +0530

    Time: 25 ms (21.59%), Space: 92.6 MB (29.56%) - LeetHub

---
+++ 3678-smallest-absent-positive-greater-than-average/3678-smallest-absent-positive-greater-than-average.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int smallestAbsent(vector<int>& nums) {
+        int n = nums.size();
+        int sum  = 0;
+        for(auto it : nums){
+            sum += it;
+        }
+        int ans = 0;
+        double ave = sum/n;
+        if(ave < 0) ave = 0;
+        set<int> s;
+        for(auto it : nums){
+            s.insert(it);
+        }
+        int x = int(ave);
+        for(int i=0;i<s.size();i++){
+            x = x + 1;
+            if((s.find(x) == s.end()) && x > ave){
+                return x;
+            }
+        }
+        return 0;
+    }
+};
