02ba6a64e18aabd418ffa89f30f229a6ff728c67
metaabhay <meta29abhay@gmail.com>
2026-09-16 22:54:42 +0530

    Time: 2282 ms (5.01%), Space: 0B (100%) - LeetHub

---
+++ 1741-find-total-time-spent-by-each-employee/1741-find-total-time-spent-by-each-employee.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT 
+    event_day AS day,
+    emp_id,
+    sum(out_time - in_time) AS total_time
+FROM employees
+group by event_day,emp_id
+ORDER BY event_day,emp_id;
